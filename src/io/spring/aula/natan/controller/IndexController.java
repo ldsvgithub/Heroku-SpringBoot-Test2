ebda27897ca76ccbf7bfc164b8da6f9a84b19aac
Leandro <leandrosaviana@gmail.com>
2018-09-25 07:56:28 -0300

    Add files via upload

--- src/io/spring/aula/natan/controller/IndexController.java
+++ src/io/spring/aula/natan/controller/IndexController.java
@@ -4,12 +4,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.spring.aula.natan.entity.Pessoa;
+
 @RestController
 public class IndexController {
 	
 	@RequestMapping(value="/", method=RequestMethod.GET)
 	public String index() {
-		return "Endpoint inicial";
+		Pessoa p1 = new Pessoa();
+		p1.setNome("Leandro");
+		p1.setEndereco("rua x numero y");
+		return "Endpoint inicial " + p1.getNome() + p1.getEndereco();
 	}
 	
 }
